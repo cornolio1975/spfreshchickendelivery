5fb4b77c98462a769e18f7dd9002ee797c1d623f
cornolio1975 <108326822+cornolio1975@users.noreply.github.com>
2026-01-06 22:35:16 +0800

    Configure production env vars in ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,12 @@ module.exports = {
             args: 'start',
             env: {
                 NODE_ENV: 'production',
-                PORT: 3000
+                PORT: 3000,
+                LALAMOVE_API_KEY: 'pk_prod_860845508f4f025c73096e60998b2285',
+                LALAMOVE_API_SECRET: 'sk_prod_apDeSy1ahaol433/zI+GUXPFttHI1DqezYoLmZLxzYKXP368pMTI94PFXg5J5yVBD',
+                LALAMOVE_MARKET: 'MY_KUL',
+                LALAMOVE_BASE_URL: 'https://rest.lalamove.com',
+                NEXT_PUBLIC_API_URL: 'https://spfreshchickendelivery.com'
             },
         },
     ],
