ebf1d1216818e7c9230ea7117a2956685f006910
cornolio1975 <108326822+cornolio1975@users.noreply.github.com>
2026-01-16 16:22:20 +0800

    Fix Safari compatibility and remove Android integration

--- android/app/src/main/java/com/spfreshchicken/delivery/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.spfreshchicken.delivery;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.spfreshchicken.delivery',
-  appName: 'SP Fresh Chicken Delivery',
-  webDir: 'out'
-};
-
-export default config;
